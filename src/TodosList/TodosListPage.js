3ab19aa1c8396926b91003ef6f954989d5970485
zhaned <zhanedmond@gmail.com>
2021-03-03 22:03:36 -0800

    made pages

---
+++ src/TodosList/TodosListPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodosListPage extends Component {
+  render() {
+    return (
+      <div>
+        Todos List Page
+      </div>
+    )
+  }
+}
